3368ccb28bbc6eaa09e598f0487743d6504a1140
JulioSanchez12 <102924705+JulioSanchez12@users.noreply.github.com>
2024-04-10 22:17:31 -0400

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,10 +22,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
-        if (env('APP_ENV') == 'production') {
-            $url->forceScheme('https');
-        }
+        Paginator::useBootstrap();
     }
 }
